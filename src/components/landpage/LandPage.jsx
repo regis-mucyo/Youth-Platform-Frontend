2ce2ece50f348bedf4fde0e7fa2fa7115c41ad27
Regis Mucyo <mucyoregis250@gmail.com>
2025-09-01 20:11:08 +0200

    feat: Landpage final view

---
+++ src/components/landpage/LandPage.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import HeroSection from "./Hero";
+import HowItWorks from "./HowItWorks";
+import Trust from "./Trust";
+import Testimonials from "./Testimonials";
+import WhyJoinUs from "./WhyJoinUs";
+export default function LandPage() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <HowItWorks />
+      <Trust />
+      <Testimonials />
+      <WhyJoinUs />
+    </>
+  );
+}
