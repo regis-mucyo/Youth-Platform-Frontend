d113bc8ecda8b810901674746845a09660ae6c99
Regis Mucyo <mucyoregis250@gmail.com>
2025-09-03 15:53:50 +0200

    feat/add mentor landing page and update other components in mentee page

--- src/components/landpage/LandPage.jsx
+++ src/components/landpage/LandPage.jsx
@@ -1,15 +1,18 @@
 import Header from "./Header";
-import HeroSection from "./Hero";
+import HeroSection from "./HeroSection";
 import HowItWorks from "./HowItWorks";
 import Trust from "./Trust";
 import Testimonials from "./Testimonials";
 import WhyJoinUs from "./WhyJoinUs";
+import ImpactSection from "./ImpactSection";
 import Footer from "./Footer";
-export default function LandPage() {
+
+export default function LandPage({ role = "mentee" }) {
   return (
     <>
-      <Header />
-      <HeroSection />
+      <Header currentRole={role} />
+      <HeroSection role={role} />
+      <ImpactSection />
       <HowItWorks />
       <Trust />
       <Testimonials />
