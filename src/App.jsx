35d2f641cb62f47067e1e82977ba1b2aa4b9e602
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2025-09-04 19:22:17 +0200

    feat: market linkage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import SessionPage from "./components/mentee/SessionPage";
 import MyConnection from "./components/mentee/MyConnection"; // Make sure this is imported
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import ChatApp from "./components/mentee/Message";
+import Market from "./components/mentee/Market";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
         <Route path="/mentee/session" element={<SessionPage />} />
         <Route path="/mentee/connection" element={<MyConnection />} />
         <Route path="mentee/message" element={<ChatApp/>}/>
+        <Route path="mentee/market" element={<Market/>}/>
       </Routes>
     </BrowserRouter>
     
