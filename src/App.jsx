fe824eaae3e235c89a3621936d6b069e61589b14
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2025-09-04 14:02:21 +0200

    feat: merge the session and connection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import SessionPage from "./components/mentee/SessionPage";
+import MyConnection from "./components/mentee/MyConnection"; // Make sure this is imported
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/mentee/session" element={<SessionPage />} />
         <Route path="/mentee/connection" element={<MyConnection />} />
-        <Route path="/" element={<SessionPage />} />
       </Routes>
     </BrowserRouter>
   );
